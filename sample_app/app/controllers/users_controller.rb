11f06ce32ebfdc98878f897f6309052bc42d94a0
honda <>
2019-11-19 17:42:06 +0900

    update 9 syo

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -2,11 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
   def new
     @user = User.new
   end
-
   def create
     @user = User.new(user_params)
     if @user.save
@@ -18,7 +16,10 @@ class UsersController < ApplicationController
     end
   end
 
-  private def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -2,27 +2,29 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
+  BASE_TITLE = "Ruby on Rails Tutorial Sample App".freeze
 
-  test "should get root" do
-    get root_url
+  test "should get home" do
+    get root_path
     assert_response :success
+    assert_select "title", BASE_TITLE
   end
 
   test "should get help" do
     get help_path
     assert_response :success
+    assert_select "title", "Help | #{BASE_TITLE}"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
+    assert_select "title", "About | #{BASE_TITLE}"
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
+    assert_select "title", "Contact | #{BASE_TITLE}"
   end
 end
